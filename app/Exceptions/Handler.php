240bb2ca586f988a579a40e213395219c605b4d7
Ruslan Tagirov <rusya97@icloud.com>
2018-02-03 21:49:12 +0600

    changes middleware and handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Request;
+use Illuminate\Auth\AuthenticationException;
+use Response;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -49,5 +52,29 @@ class Handler extends ExceptionHandler
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if($request->expectsJson()) 
+        {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
+        $guard = array_get($exception->guards(), 0);
+
+        if($guard === 'admin')
+        {
+            return redirect()->guest(route('admin.login'));
+        }
+
+        return redirect()->guard(route('login'));
     }
 }
